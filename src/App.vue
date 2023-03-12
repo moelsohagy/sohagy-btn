5206f5daf0cbb2be8d4dd6096c76e45fce1cbf25
mohamed-elsohagy <mohamed.elsohagy@dafa.sa>
2023-03-12 16:08:50 +0200

    add app vue file & main js

---
+++ src/App.vue
@@ -0,0 +1,12 @@
+<script setup>
+import SohagyBtn from './components/SohagyBtn.vue'
+</script>
+
+<template>
+  <div class="btn">
+    <SohagyBtn title="abc" subtitle="asd">zzzzzzzz</SohagyBtn>
+  </div>
+</template>
+
+<style scoped>
+</style>
